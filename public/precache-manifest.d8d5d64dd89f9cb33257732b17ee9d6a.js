eee9765332558d2f8ec174254277f6f22ec556d3
eidytrinidad <eidyjtf@gmail.com>
2020-11-05 15:17:46 -0400

    Backend con FrontEnd

---
+++ public/precache-manifest.d8d5d64dd89f9cb33257732b17ee9d6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cae721d20dac18f4451df99d41a46a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "016fdc4853f8909d9e7f",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "9cae777dadd728466aa2",
+    "url": "/static/css/main.d8c1bd37.chunk.css"
+  },
+  {
+    "revision": "016fdc4853f8909d9e7f",
+    "url": "/static/js/2.21e1c2ab.chunk.js"
+  },
+  {
+    "revision": "62dc9784057de1bc85a8684fec455f1b",
+    "url": "/static/js/2.21e1c2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cae777dadd728466aa2",
+    "url": "/static/js/main.1789d788.chunk.js"
+  },
+  {
+    "revision": "738c1a5c78ca1595c023",
+    "url": "/static/js/runtime-main.4f3b810c.js"
+  }
+]);
